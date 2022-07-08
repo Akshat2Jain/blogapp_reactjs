a3ff957446a173901a87bddb49ff333bcf92bb14
FloatingDorito <52376777+FloatingDorito@users.noreply.github.com>
2022-07-08 09:07:58 +0800

    Update Page 404 Not Found

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+    return (
+      <div className="not-found">
+           <h2>Sorry</h2>
+        <p>That page cannot be found</p>
+      </div>
+    );
+  }
+export default NotFound;
